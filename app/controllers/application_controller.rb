3b9790614398d801da8bed9d78a41f513eb7dc8f
João Pedro Lopes <janesgbs@gmail.com>
2021-08-27 09:35:20 -0400

    refac: changed set_current_user and require_user_logged_in methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
 
   def set_current_user
     if session[:current_user]
-      Current.user = User.find_by(id: session[:current_user])
+      Current.user = User.find_by_id(session[:current_user])
     end
   end
 
   def require_user_logged_in
-    redirect_to sign_in_path, alert: "You must be signed in to access this content"
+    if !Current.user.present?
+      redirect_to sign_in_path, alert: "You must be signed in to access this content"
+    end
   end
 end
