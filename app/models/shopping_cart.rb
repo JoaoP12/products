04620bd13345e7debe7d933525a45b615a2b306a
João Pedro Lopes <janesgbs@gmail.com>
2021-09-01 10:24:06 -0400

    chore: associated relations between user, shopping_cart and shopping_cart_product models

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,2 +1,4 @@
 class ShoppingCart < ApplicationRecord
+  belongs_to :user
+  has_many :shopping_cart_products, dependent: :destroy
 end
--- app/models/shopping_cart_product.rb
+++ app/models/shopping_cart_product.rb
@@ -1,2 +1,3 @@
 class ShoppingCartProduct < ApplicationRecord
+  belongs_to :shopping_cart
 end
