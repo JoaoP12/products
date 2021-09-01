2bcc9626d2cde1de68478c312a66a386a7a46b2a
João Pedro Lopes <janesgbs@gmail.com>
2021-09-01 07:33:33 -0400

    refac: replaced code from previous authentication handling for devise's actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
-
-  def set_current_user
-    if session[:current_user]
-      Current.user = User.find_by_id(session[:current_user])
-    end
-  end
-
-  def require_user_logged_in
-    if !Current.user.present?
-      redirect_to sign_in_path, alert: "You must be signed in to access this content"
-    end
-  end
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
 end
