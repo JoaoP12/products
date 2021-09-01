f8c2541594584db920bc28e4f54a974e0a1c7b2f
João Pedro Lopes <janesgbs@gmail.com>
2021-09-01 07:40:22 -0400

    refac: deleted old authentication routes and added devise's routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "home#index"
 
   get "products", to: "products#index"
@@ -6,17 +8,5 @@ Rails.application.routes.draw do
   patch "products/edit", to: "products#edit", as: :edit_product
   delete "products/:id", to: "products#delete", as: :delete_product
 
-  get "sign-up", to: "registrations#new"
-  post "sign-up", to: "registrations#create"
-  get "registration-confirmation/:id", to: "registrations#confirm_email",
-    as: :registration_confirmation
-
-  get "sign-in", to: "sessions#new"
-  post "sign-in", to: "sessions#create"
-  delete "sign-out", to: "sessions#destroy"
-
-  resources :password_resets
-
   get "shopping-cart", to: "shopping_cart#show"
-
 end
