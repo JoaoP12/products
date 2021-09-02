d66e48879b6ed7448fe4475eb03916c4ac179991
João Pedro Lopes <janesgbs@gmail.com>
2021-09-02 10:00:27 -0400

    feat: added enum attribute called role, to manage the users' roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  enum role: {
+    user: 0,
+    moderator: 1,
+    admin: 2
+  }
+
   validates :email, presence: true, format: {
     with: /\A[^@\s]+@[^@\s]+\z/,
     message: "must be a valid email address"
