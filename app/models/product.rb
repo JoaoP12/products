b0d3c9753a0eb2b36ea83e57721bbe8f5ee38955
João Pedro Lopes <janesgbs@gmail.com>
2021-09-03 10:49:12 -0400

    chore: added searchkick to the model
    Added word_middle and search_data methods to enable elasticsearch indexing to the model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,17 @@
 class Product < ApplicationRecord
   include ActiveSupport::NumberHelper
   validates :description, :price, :title, :presence => true
+
+  searchkick word_middle: [:title, :description]
+
   def price_formatted
     number_to_currency(self.price, precision: 2, unit: 'R$ ')
+  end
+
+  def search_data
+    {
+      title: title,
+      description: description
+    }
   end
 end
